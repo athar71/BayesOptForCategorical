df0e270e0d01ff4197d3c414045a2c79b05617f2
athar71 <athar@bu.edu>
2019-07-08 12:36:49 -0400

    PC change

--- mainmodel1.py
+++ mainmodel1.py
@@ -2,7 +2,9 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Jul  8 11:31:10 2019
-
 @author: athar
+
+This code is modling bayseian optimization for the cases with categorical variables
 """
 
+import numpy as np
